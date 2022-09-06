1cbd4c95a609ef98f4d8687fa663e023bba203d1
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-06 09:21:56 -0500

    readded server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+const mongoose = require('mongoose')
+
+const bookRoute = require('./routes/bookRoute')
+const bookPostRoute = require('./routes/bookPostRoute')
+const bookDeleteRoute = require('./routes/bookDeleteRoute')
+const home = require('./routes/home')
+const test = require('./routes/test')
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, ()=>{
+  console.log('connected')
+},
+err=>console.log(err))
+
+app.use(home)
+app.use(test)
+app.use(bookRoute)
+app.use(bookPostRoute)
+app.use(bookDeleteRoute)
+
+
+app.listen(process.env.PORT, ()=>console.log('server up and running'))
