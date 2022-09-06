07a5ba592df140348408c54faab9bc3919976b43
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-06 15:25:44 -0500

    new db due to security breach

--- funcs/seedBooks.js
+++ funcs/seedBooks.js
@@ -2,23 +2,23 @@ const Book = require('../models/Book')
 
 const seedBooks = async()=>{
   try {
-    const book1 = Book.create({
+    const book1 = await Book.create({
       title:"Children of Blood and Bone",
       image: "https://images-na.ssl-images-amazon.com/images/I/A1e37vzc5VL.jpg",
       description: "An amazing book by Tomi Adeyemi",
-      status: "isAvailable"
+      status: true
     })
-    const book2 = Book.create({
+    const book2 = await Book.create({
       title:"Children of Virtue and Vengeance",
       image: "https://images-na.ssl-images-amazon.com/images/I/91TVZ3soQwL.jpg",
       description: "An amazing second book by Tomi Adeyemi",
-      status: "isAvailable"
+      status: true
     })
-    const book3 = Book.create({
+    const book3 = await Book.create({
       title:"Children of ....",
       image: "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg",
       description: "An amazing third book by Tomi Adeyemi",
-      status: "notAvailable"
+      status: false
     })
     await book1.save()
     await book2.save()
