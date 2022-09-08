a252ca80ae54cb62d0e299628f2a7d73c261dfbf
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-08 08:46:49 -0500

    tried backend auth0

--- server.js
+++ server.js
@@ -2,23 +2,27 @@
 
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json())
 const mongoose = require('mongoose')
 
 const booksRoutes = require('./routes/booksRoutes')
 const home = require('./routes/home')
-const test = require('./routes/test')
+const test = require('./routes/test');
+const verifyJwt = require('./auth0/verifyJwt')
+const errMiddleware = require('./funcs/error')
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, ()=>{
   console.log('connected')
 },
 err=>console.log(err))
 
+app.use(cors());
+app.use(bodyParser.json())
+app.use(verifyJwt)
+app.use(errMiddleware)
 app.use(home)
 app.use(test)
 app.use('/books', booksRoutes)
